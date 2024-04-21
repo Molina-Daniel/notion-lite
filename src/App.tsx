158ee172ae87f105b1e3b5eba040ae77ac94e3b6
Daniel Molina <danielmolina.dev@gmail.com>
2024-04-21 13:26:55 +0200

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import { Page } from "./Page/Page";
 import { AppStateProvider } from "./state/AppStateContext";
 import { createPage } from "./utils/page";
@@ -5,11 +6,31 @@ import "./App.css";
 
 const initialState = createPage();
 
+const Auth = () => {
+  return <div>Auth</div>;
+};
+
 function App() {
   return (
-    <AppStateProvider initialState={initialState}>
-      <Page />
-    </AppStateProvider>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/"
+        element={
+          <AppStateProvider initialState={initialState}>
+            <Page />
+          </AppStateProvider>
+        }
+      />
+      <Route
+        path="/:id"
+        element={
+          <AppStateProvider initialState={initialState}>
+            <Page />
+          </AppStateProvider>
+        }
+      />
+    </Routes>
   );
 }
 
